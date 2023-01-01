1cd07f551b5b6949af6da02d6df20cdb874d6a8b
isabellaattisano <85297147+isabellaattisano@users.noreply.github.com>
2023-01-01 15:52:03 -0500

    Create StoreData.sql

---
+++ StoreData.sql
@@ -0,0 +1,205 @@
+--delete from product;
+INSERT into product VALUES(seqProduct.nextVal, 'Square Neck Blouse', 'tops', 19.95, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'Oversized Sweater', 'tops', 49.95, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'High-Waisted Pants', 'bottoms', 49.95, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'Plaid Mini Skirt', 'bottoms', 25.00, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'Jacket', 'outerwear', 49.95, 2);
+INSERT into product VALUES(seqproduct.nextVal, '4pck Scrunchies', 'accessories', 14.95, 2);
+
+INSERT into product VALUES(seqproduct.nextVal, 'Pumpkin Spice', 'candles', 10.99, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'Glitter Pink', 'phone cases', 16.99, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'Monopoly', 'board games', 25.95, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'Grey Mug LARGE', 'mugs', 12.99, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'Stay Fine and Drink Wine', 'posters', 9.99, 2);
+
+INSERT into product VALUES(seqproduct.nextVal, 'Cream Cable Knit Sweater', 'tops', 35.50, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Paisley Midi Skirt', 'bottoms', 55.95, 34);
+INSERT into product VALUES(seqproduct.nextVal, 'Black Head Band', 'accessories', 8.45, 14);
+INSERT into product VALUES(seqproduct.nextVal, '3pck Pink Claw Clips', 'accessories', 10.00, 25);
+INSERT into product VALUES(seqproduct.nextVal, 'Black Velvet Strapless Dress', 'dresses', 75.50, 12);
+
+INSERT into product VALUES(seqproduct.nextVal, 'Vintage Coca Cola Santa Tee', 'tops', 39.00, 50);
+INSERT into product VALUES(seqproduct.nextVal, 'Brown Low-Rise Cargo Pant', 'bottoms', 79.00, 75);
+INSERT into product VALUES(seqproduct.nextVal, 'Grey Lace-Trim Lounge Pants', 'bottoms', 49.00, 40);
+INSERT into product VALUES(seqproduct.nextVal, 'Grey Lace-Trim Loung Top', 'tops', 31.00, 40);
+INSERT into product VALUES(seqproduct.nextVal, 'Black Leather Flare Pant', 'bottoms', 99.00, 30);
+
+INSERT into product VALUES(seqproduct.nextVal, 'Champion Pink Hoodie Sweatshirt', 'tops', 60.00, 50);
+INSERT into product VALUES(seqproduct.nextVal, 'Flannel Distressed Button-Down Shirt', 'tops', 49.00, 80);
+INSERT into product VALUES(seqproduct.nextVal, 'Cropped Fitted Pinstripe Vest', 'tops', 49.00, 25);
+INSERT into product VALUES(seqproduct.nextVal, 'Read Lace Babydoll Mini Dress', 'dresses', 69.00, 30);
+INSERT into product VALUES(seqproduct.nextVal, 'Cowl Neck Slip Dress', 'dresses', 49.00, 30);
+
+INSERT into product VALUES(seqproduct.nextVal, 'Sequin Mini Dress', 'dresses', 79.00, 30);
+INSERT into product VALUES(seqproduct.nextVal, 'Lace-Trim Floral Midi Dress', 'dresses', 59.00, 30);
+INSERT into product VALUES(seqproduct.nextVal, 'Faux Leather Blazer', 'outerwear', 99.00, 25);
+INSERT into product VALUES(seqproduct.nextVal, 'North Face 1996 Retro Nutpse Jacket', 'outerwear', 320.00, 40);
+INSERT into product VALUES(seqproduct.nextVal, 'Colorblock Windbreaker Jacket', 'outerwear', 69.00, 40);
+
+INSERT into product VALUES(seqproduct.nextVal, 'Corduroy Bomber Jacket', 'outerwear', 110.00, 40);
+INSERT into product VALUES(seqproduct.nextVal, 'Light Blue Beanie', 'accessories', 27.00, 30);
+INSERT into product VALUES(seqproduct.nextVal, 'Olive Cable Knit Sock', 'accessories', 16.00, 30);
+INSERT into product VALUES(seqproduct.nextVal, 'BAGGU Big Reusable Tote Bag', 'accessories', 18.00, 30);
+INSERT into product VALUES(seqproduct.nextVal, 'Black Bow Seam Tight', 'accessories', 17.00, 30);
+
+INSERT into product VALUES(seqproduct.nextVal, 'BAGGU Packing Cube Set', 'accessories', 34.00, 30);
+INSERT into product VALUES(seqproduct.nextVal, 'Silver Heart Drop Earring', 'accessories', 17.00, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'UGG White Faux Fur Earmuff', 'accessories', 55.00, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Mini Blue Heart Clip Set', 'accessories', 10.00, 15);
+INSERT into product VALUES(seqproduct.nextVal, 'Brown Multi Cat-Eye Sunglasses', 'accessories', 17.00, 15);
+
+INSERT into product VALUES(seqproduct.nextVal, 'SZA The Party Is Over Poster', 'posters', 5.91, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'The Weeknd House of Balloons Album Poster', 'posters', 5.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Capri Blue Volcano Mercury Glass Jar Candle', 'candles', 25.20, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Wildflower Strawberry Fields iPhone Case', 'phone cases', 35.00, 15);
+INSERT into product VALUES(seqproduct.nextVal, 'Wildflower Polka Dot iPhone Case', 'phone cases', 37.00, 15);
+
+INSERT into product VALUES(seqproduct.nextVal, 'Pumpkin Spice', 'candles', 10.99, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'Jacket', 'outerwear', 49.95, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'Colorblock Windbreaker Jacket', 'outerwear', 69.00, 40);
+INSERT into product VALUES(seqproduct.nextVal, '5-Pack Rings', 'accessories', 34.00, 20);
+
+INSERT into product VALUES(seqproduct.nextVal, 'Winter Wonderland', 'candles', 10.99, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'Metallica', 'posters', 5.99, 2);
+INSERT into product VALUES(seqproduct.nextVal, 'Silver Hoops - 10pack', 'accessories', 20.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Sugar and Spice and Everything Nice', 'candles', 10.99, 20);
+
+INSERT into product VALUES(seqproduct.nextVal, 'Vanilla Birch', 'candles', 10.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Winter Mantel', 'candles', 10.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Christmas Cookie', 'candles', 10.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Winter Forest', 'candles', 10.99, 20);
+
+INSERT into product VALUES(seqproduct.nextVal, 'Forest Green Oversized Sweater', 'tops', 49.95, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Washed Black Cargo Pocket Jeans', 'bottoms', 65.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Gold Basic Aviator Sunglasses', 'accessories', 12.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Chocolate Satin Corset', 'tops', 25.99, 20);
+
+INSERT into product VALUES(seqproduct.nextVal, 'White Bardot Lace Frill Hem Midi Dress', 'dresses', 70.00, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Stone Maxi Length Hooded Puffer', 'outerwear', 65.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Pink Ribbed Halterneck Bodycon Dress', 'dresses', 45.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Cream Textured Rib High Waist Flared Pants', 'bottoms', 45.99, 20);
+
+INSERT into product VALUES(seqproduct.nextVal, 'Silver Interlocking Square Drop Earrings', 'accessories', 12.00, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'White Over Baguette Shoulder Bag', 'accessories', 35.00, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'Pink Velvet Lace Up Top', 'tops', 35.99, 20);
+INSERT into product VALUES(seqproduct.nextVal, 'J Cole Metal Poster', 'posters', 25.99, 20);
+
+select * from product
+-------------------------------------------------------------------------------------------
+--Display Products 
+
+--all products 
+select * from product;
+
+--Select statement shows products in stock
+SELECT Pname, Price, ProductID FROM product WHERE pquantity>0; 
+
+-- View all out of stock products 
+select * from product where pquantity = 0;
+
+-------------------------------------------------------------------------------------------
+-- Update stored quantity
+
+-- Square neck blouse updated to a stored quantity of 30
+UPDATE product 
+SET pquantity = 200
+WHERE ProductID = 1;
+
+-- Oversized Sweater updated to a stored quantity of 0
+UPDATE product 
+SET pquantity = 110
+WHERE ProductID = 2;
+
+-- High Waisted Pants updated to stored quantity of 50
+UPDATE product
+SET pquantity = 50
+WHERE ProductID = 3;
+
+-- Plaid Mini Skirt updated to stored quantity of 100
+UPDATE product
+SET pquantity = 140
+WHERE ProductID = 4;
+
+-- Jacket updated to stored quantity of 15
+UPDATE product 
+SET pquantity = 190
+WHERE ProductID = 5;
+
+-- 4 Pack Scunchies updated to stored quantity of 90
+UPDATE product 
+SET pquantity = 90
+WHERE ProductID = 6;
+
+select * from product
+
+-------------------------------------------------------------------------------------------
+--Filter (clothing v. homegoods) by Price
+
+--Price High to Low
+select * from product where ptype not in ('mugs', 'board games', 'posters', 'phone cases', 'candles') and pquantity > 0 order by price asc;
+select * from product where ptype in ('mugs', 'board games', 'posters', 'phone cases', 'candles') and pquantity > 0 order by price asc;
+
+
+--Price Low to High 
+select * from product where ptype not in ('mugs', 'board games', 'posters', 'phone cases', 'candles') and pquantity > 0 order by price asc;
+select * from product where ptype in ('mugs', 'board games', 'posters', 'phone cases', 'candles') and pquantity > 0 order by price asc;
+
+
+--Perfect gifts under 30$
+SELECT PName, ProductID, Price FROM product WHERE Price < 30 and pquantity > 0;
+
+--Nice gifts over $60
+SELECT PName, ProductID, Price FROM product WHERE Price > 60 and pquantity > 0;
+
+--Favorites
+SELECT PName FROM product p and favorites f where p.productid=f.productid;
+
+select * from product where pquantity > 0 and ptype = 'tops' and price<=30 and pquantity > 0;
+
+--FILTER BY TYPE 
+
+--view homegoods
+select * from product where ptype in ('mugs', 'board games', 'posters', 'phone cases') and pquantity > 0;
+
+--view clothing
+select * from product where ptype not in ('mugs', 'board games', 'posters', 'phone cases') and pquantity > 0;
+
+select * from product where ptype = 'mugs' and pquantity > 0;
+select * from product where ptype = 'board games' and pquantity > 0;
+select * from product where ptype = 'posters' and pquantity > 0;
+select * from product where ptype = 'phone cases' and pquantity > 0;
+select * from product where ptype = 'candles' and pquantity > 0;
+
+
+select * from product where ptype = 'tops' and pquantity > 0;
+select * from product where ptype = 'bottoms' and pquantity > 0;
+select * from product where ptype = 'accessories' and pquantity > 0;
+select * from product where ptype = 'outerwear' and pquantity > 0;
+select * from product where ptype = 'dresses' and pquantity > 0;
+
+
+-------------------------------------------------------------------------------------------
+-- Select Statement indicating the quantity of products there are of each type 
+
+SELECT ptype, count(ptype) as Quantity
+from product
+group by ptype;
+
+-------------------------------------------------------------------------------------------
+
+--get all customer info
+SELECT accountid, fname || ' ' || lname as Name, email FROM CUSTOMER;
+
+-------------------------------------------------------------------------------------------
+--Get reviews for favorites
+SELECT p.pname, r.rating
+from product p and reviews r
+where p.productid=r.productid
+group by p.pname;
+
+-------------------------------------------------------------------------------------------
+--view recently purchased
+SELECT p.pname, p.psize, p.pquantity, i.invoiceid
+from product p and invoice_products i
+where p.productid=i.productid
+group by i.invoiceid;
